c8515245ebd7e8e38048ad65ce435435ab616488
Chiracolivier <chiracolivier28@gmail.com>
2019-08-21 21:03:43 +0200

    amélioration de la servlet CreationClient(.java).

--- src/main/CreationClient.java
+++ src/main/CreationClient.java
@@ -10,19 +10,32 @@ import javax.servlet.http.HttpServletResponse;
 import com.sdzee.tp.beans.Client;
 
 public class CreationClient extends HttpServlet {
+    /* Constantes */
+    public static final String CHAMP_NOM       = "nomClient";
+    public static final String CHAMP_PRENOM    = "prenomClient";
+    public static final String CHAMP_ADRESSE   = "adresseClient";
+    public static final String CHAMP_TELEPHONE = "telephoneClient";
+    public static final String CHAMP_EMAIL     = "emailClient";
+
+    public static final String ATT_CLIENT      = "client";
+    public static final String ATT_MESSAGE     = "message";
+    public static final String ATT_ERREUR      = "erreur";
+
+    public static final String VUE             = "/afficherClient.jsp";
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /*
          * Récupération des données saisies, envoyées en tant que paramètres de
          * la requête GET générée à la validation du formulaire
          */
-        String nom = request.getParameter( "nomClient" );
-        String prenom = request.getParameter( "prenomClient" );
-        String adresse = request.getParameter( "adresseClient" );
-        String telephone = request.getParameter( "telephoneClient" );
-        String email = request.getParameter( "emailClient" );
+        String nom = request.getParameter( CHAMP_NOM );
+        String prenom = request.getParameter( CHAMP_PRENOM );
+        String adresse = request.getParameter( CHAMP_ADRESSE );
+        String telephone = request.getParameter( CHAMP_TELEPHONE );
+        String email = request.getParameter( CHAMP_EMAIL );
 
         String message;
+        boolean erreur;
         /*
          * Initialisation du message à afficher : si un des champs obligatoires
          * du formulaire n'est pas renseigné, alors on affiche un message
@@ -30,8 +43,10 @@ public class CreationClient extends HttpServlet {
          */
         if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() ) {
             message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerClient.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'un client.";
+            erreur = true;
         } else {
             message = "Client créé avec succès !";
+            erreur = false;
         }
         /*
          * Création du bean Client et initialisation avec les données récupérées
@@ -44,10 +59,11 @@ public class CreationClient extends HttpServlet {
         client.setEmail( email );
 
         /* Ajout du bean et du message à l'objet requête */
-        request.setAttribute( "client", client );
-        request.setAttribute( "message", message );
+        request.setAttribute( ATT_CLIENT, client );
+        request.setAttribute( ATT_MESSAGE, message );
+        request.setAttribute( ATT_ERREUR, erreur );
 
         /* Transmission à la page JSP en charge de l'affichage des données */
-        this.getServletContext().getRequestDispatcher( "/afficherClient.jsp" ).forward( request, response );
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 }
