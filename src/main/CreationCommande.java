45f52b4b60bc0114c850f3664ca6e95b54049ecf
Chiracolivier <chiracolivier28@gmail.com>
2019-08-21 21:05:02 +0200

    amélioration de la servlet CreationCommande(.java).

--- src/main/CreationCommande.java
+++ src/main/CreationCommande.java
@@ -15,37 +15,59 @@ import com.sdzee.tp.beans.Client;
 import com.sdzee.tp.beans.Commande;
 
 public class CreationCommande extends HttpServlet {
+    /* Constantes */
+    public static final String CHAMP_NOM              = "nomClient";
+    public static final String CHAMP_PRENOM           = "prenomClient";
+    public static final String CHAMP_ADRESSE          = "adresseClient";
+    public static final String CHAMP_TELEPHONE        = "telephoneClient";
+    public static final String CHAMP_EMAIL            = "emailClient";
+
+    public static final String CHAMP_DATE             = "dateCommande";
+    public static final String CHAMP_MONTANT          = "montantCommande";
+    public static final String CHAMP_MODE_PAIEMENT    = "modePaiementCommande";
+    public static final String CHAMP_STATUT_PAIEMENT  = "statutPaiementCommande";
+    public static final String CHAMP_MODE_LIVRAISON   = "modeLivraisonCommande";
+    public static final String CHAMP_STATUT_LIVRAISON = "statutLivraisonCommande";
+
+    public static final String ATT_COMMANDE           = "commande";
+    public static final String ATT_MESSAGE            = "message";
+    public static final String ATT_ERREUR             = "erreur";
+
+    public static final String FORMAT_DATE            = "dd/MM/yyyy HH:mm:ss";
+
+    public static final String VUE                    = "/afficherCommande.jsp";
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /*
          * Récupération des données saisies, envoyées en tant que paramètres de
          * la requête GET générée à la validation du formulaire
          */
-        String nom = request.getParameter( "nomClient" );
-        String prenom = request.getParameter( "prenomClient" );
-        String adresse = request.getParameter( "adresseClient" );
-        String telephone = request.getParameter( "telephoneClient" );
-        String email = request.getParameter( "emailClient" );
+        String nom = request.getParameter( CHAMP_NOM );
+        String prenom = request.getParameter( CHAMP_PRENOM );
+        String adresse = request.getParameter( CHAMP_ADRESSE );
+        String telephone = request.getParameter( CHAMP_TELEPHONE );
+        String email = request.getParameter( CHAMP_EMAIL );
 
         /* Récupération de la date courante */
         DateTime dt = new DateTime();
-        /* Conversion de la date en String selon le format défini */
-        DateTimeFormatter formatter = DateTimeFormat.forPattern( "dd/MM/yyyy HH:mm:ss" );
+        /* Conversion de la date en String selon le format choisi */
+        DateTimeFormatter formatter = DateTimeFormat.forPattern( FORMAT_DATE );
         String date = dt.toString( formatter );
         double montant;
         try {
             /* Récupération du montant */
-            montant = Double.parseDouble( request.getParameter( "montantCommande" ) );
+            montant = Double.parseDouble( request.getParameter( CHAMP_MONTANT ) );
         } catch ( NumberFormatException e ) {
             /* Initialisation à -1 si le montant n'est pas un nombre correct */
             montant = -1;
         }
-        String modePaiement = request.getParameter( "modePaiementCommande" );
-        String statutPaiement = request.getParameter( "statutPaiementCommande" );
-        String modeLivraison = request.getParameter( "modeLivraisonCommande" );
-        String statutLivraison = request.getParameter( "statutLivraisonCommande" );
+        String modePaiement = request.getParameter( CHAMP_MODE_PAIEMENT );
+        String statutPaiement = request.getParameter( CHAMP_STATUT_PAIEMENT );
+        String modeLivraison = request.getParameter( CHAMP_MODE_LIVRAISON );
+        String statutLivraison = request.getParameter( CHAMP_STATUT_LIVRAISON );
 
         String message;
+        boolean erreur;
         /*
          * Initialisation du message à afficher : si un des champs obligatoires
          * du formulaire n'est pas renseigné, alors on affiche un message
@@ -54,8 +76,10 @@ public class CreationCommande extends HttpServlet {
         if ( nom.trim().isEmpty() || adresse.trim().isEmpty() || telephone.trim().isEmpty() || montant == -1
                 || modePaiement.isEmpty() || modeLivraison.isEmpty() ) {
             message = "Erreur - Vous n'avez pas rempli tous les champs obligatoires. <br> <a href=\"creerCommande.jsp\">Cliquez ici</a> pour accéder au formulaire de création d'une commande.";
+            erreur = true;
         } else {
             message = "Commande créée avec succès !";
+            erreur = false;
         }
         /*
          * Création des beans Client et Commande et initialisation avec les
@@ -78,10 +102,11 @@ public class CreationCommande extends HttpServlet {
         commande.setStatutLivraison( statutLivraison );
 
         /* Ajout du bean et du message à l'objet requête */
-        request.setAttribute( "commande", commande );
-        request.setAttribute( "message", message );
+        request.setAttribute( ATT_COMMANDE, commande );
+        request.setAttribute( ATT_MESSAGE, message );
+        request.setAttribute( ATT_ERREUR, erreur );
 
         /* Transmission à la page JSP en charge de l'affichage des données */
-        this.getServletContext().getRequestDispatcher( "/afficherCommande.jsp" ).forward( request, response );
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 }
