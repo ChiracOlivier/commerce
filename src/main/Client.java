4c3bbae6c21c0a1c7dc7e3df00811d4f29259d27
Chiracolivier <chiracolivier28@gmail.com>
2019-08-22 23:37:23 +0200

    amelioration de menu.jsp

--- src/main/Client.java
+++ src/main/Client.java
@@ -16,35 +16,4 @@ public class Client {
         return nom;
     }
 
-    public void setPrenom( String prenom ) {
-        this.prenom = prenom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setAdresse( String adresse ) {
-        this.adresse = adresse;
-    }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone( String telephone ) {
-        this.telephone = telephone;
-    }
-
-    public void setEmail( String email ) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-}
+   
